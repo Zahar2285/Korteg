ccdfebf652fc9b0aee40ad17160f27866d18326c
Zahar2285 <Zahar2285@mail.ru>
2024-08-25 16:14:24 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var=(1,5,'cod',0.1)
 print(type(immutable_var))
-#immutable_var[0]=7 не изменяемые элементы кортежа
+#immutable_var[0]=7 данные не изменяются, так как в кортеже не возможно изменять данные это заложенно в структуру
 print(immutable_var)
 mutable_list= [1,2,'a','b']
 print(mutable_list)
